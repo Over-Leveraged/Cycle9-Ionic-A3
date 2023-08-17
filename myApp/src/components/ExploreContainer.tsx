7246c5bbc0732b32b7fd2dc0ad88291b6ce2ecc9
Akiel Walsh <71809218+Over-Leveraged@users.noreply.github.com>
2023-08-17 00:42:52 -0500

    Achieved Basic Navigation

---
+++ myApp/src/components/ExploreContainer.tsx
@@ -0,0 +1,19 @@
+import './ExploreContainer.css';
+import { Link } from 'react-router-dom';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      
+      <strong>Meet Akiel Walsh</strong>
+      
+      {/* Add an Image of Akiel */}
+      <p>He is a me</p>
+
+    </div>
+  );
+};
+
+export default ExploreContainer;
